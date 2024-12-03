cd58e4cbb77b884e2b08fe7340efb23fbeff1d74
Ashok Sethy <ima.ashok@gmail.com>
2024-12-03 23:45:50 +0530

    Popualating all ToDO items on home page

--- src/app/MyComponents/add-todo/add-todo.component.ts
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { Todo } from '../../Todo';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-add-todo',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-todo.component.html',
   styleUrl: './add-todo.component.css',
   standalone: true, // This indicates a standalone component
 })
 export class AddTodoComponent {
   todos: Todo[] = [];
+  constructor(){
+    this.todos =[
+      {
+        sno:1,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:2,
+        title: "This is title 2",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:3,
+        title: "This is title 3",
+        desc: "Description",
+        active: true
+      },
+    ]
+  }
 
 }
