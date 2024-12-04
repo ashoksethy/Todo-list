35184ef181a773e9358b288cc88963c7fa1978b0
Ashok Sethy <ima.ashok@gmail.com>
2024-12-04 23:28:20 +0530

    Added New Component for Todo List

---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../../../../src/app/Todo';
+@Component({
+  selector: 'app-todo-item',
+  imports: [],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+  @Input() todo: Todo | undefined;
+}
