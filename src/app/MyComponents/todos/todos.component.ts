6a3b7e0a10114338d60285063c5b28acfebb15dd
Ashok Sethy <ima.ashok@gmail.com>
2024-12-07 18:32:44 +0530

    Delete todo with satic value and addTodo Form added

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { Todo } from '../../Todo';
 import { CommonModule } from '@angular/common';
 import { TodoItemComponent } from "../todo-item/todo-item.component";
+import { AddTodoComponent } from "../add-todo/add-todo.component";
 @Component({
-  selector: 'app-add-todo',
-  imports: [CommonModule, TodoItemComponent],
-  templateUrl: './add-todo.component.html',
-  styleUrl: './add-todo.component.css',
+  selector: 'app-todos',
+  imports: [CommonModule, TodoItemComponent, AddTodoComponent],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css',
   standalone: true, // This indicates a standalone component
 })
-export class AddTodoComponent {
+export class TodoComponent {
   todos: Todo[] = [];
   constructor(){
     this.todos =[
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component'; 
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { TodoComponent } from "./MyComponents/todos/todos.component"; 
 
 @Component({
   // declarations: [
   //   AppComponent,
   //   AddTodoComponent // Declare your new component here
   // ],
-  imports: [AddTodoComponent], // Import the standalone component here
+  imports: [TodoComponent], // Import the standalone component here
   providers: [],
   selector: 'app-root',
   // imports: [RouterOutlet],
