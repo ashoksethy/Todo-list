6793c6f9d5124db3f00d3b798af1e14a330a7a4a
Ashok Sethy <ima.ashok@gmail.com>
2024-12-08 21:57:22 +0530

    UI changes

--- src/app/MyComponents/add-todo/add-todo.component.ts
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { TodoItemComponent } from "../todo-item/todo-item.component";
 @Component({
   selector: 'app-add-todo',
-  imports: [CommonModule, TodoItemComponent],
+  imports: [CommonModule],
   templateUrl: './add-todo.component.html',
   styleUrl: './add-todo.component.css',
   standalone: true, // This indicates a standalone component
